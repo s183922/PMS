033b32cc8148802d1c7d84c067e3268257eb5e8d
Peter Grønning <s183922@win.dtu.dk>
2020-10-06 20:03:16 +0200

    Assignment1

---
+++ Assignment1/fwdsub.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <math.h>
+int fwdsub(
+  unsigned long n,
+  double alpha,
+  double **R,  /* two-dimensional array, row-major */
+  double *b    /* one-dimensional array */
+  ){
+  /* Insert your code here */
+}
